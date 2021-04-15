3e338f7e9d2ae8dede5cc2af05e861196999308e
sukumarinapp <sukumar.inapp@gmail.com>
2021-04-15 18:15:52 +0530

    add report

--- admin/header.php
+++ admin/header.php
@@ -32,7 +32,7 @@
           <!-- User Account: style can be found in dropdown.less -->
 		  <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-             <img src="photo/user.jpg" class="user-image" alt="User Image">
+             <img src="photo/logouser.jpg" class="user-image" alt="User Image">
               <span class="hidden-xs"> <?php echo $_SESSION['full_name']; ?></span>
             </a>
 
